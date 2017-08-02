1f395079615cab85f952fcfc91158f7583815ac4
khatamoto-cq <k.hatamoto@caraquri.com>
2017-08-02 12:25:30 +0900

    APIをバージョニングした

---
+++ app/controllers/api/v1/api_base_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class ApiBaseController < ApplicationController
+    end
+  end
+end
---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::BooksController < Api::V1::ApiBaseController
+  before_action :set_book, only: [:show, :update]
+
+  def index
+    @books = Book.all
+
+    render json: @books
+  end
+
+  def show
+    render json: @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:image_url, :name, :price, :purchage_date)
+  end
+end

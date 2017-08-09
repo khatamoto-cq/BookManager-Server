412655938909c92eef452a4d84b1a42546fd8683
khatamoto-cq <k.hatamoto@caraquri.com>
2017-08-09 09:35:17 +0900

    Knockで生成したコントローラをapi用にする為にネームスペースとルーティングを調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post 'user_token' => 'user_token#create'
+      post 'sign_up' => 'user_token#create'
       resources :books, except: :destroy
     end
   end

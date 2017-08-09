35a62b10cc2b545a790a6bf70580e145349ba5b9
khatamoto-cq <k.hatamoto@caraquri.com>
2017-08-09 09:33:29 +0900

    Userモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802021457) do
+ActiveRecord::Schema.define(version: 20170809002721) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "image_url"
     t.string "name"
     t.integer "price"
     t.datetime "purchage_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

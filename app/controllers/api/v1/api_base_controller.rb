ce6c5f074bc9d1270cee83feba56f9d7263222cb
khatamoto-cq <k.hatamoto@caraquri.com>
2017-08-09 10:16:50 +0900

    ログイン機能を実装

--- app/controllers/api/v1/api_base_controller.rb
+++ app/controllers/api/v1/api_base_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ApiBaseController < ApplicationController
+      include Knock::Authenticable
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'user_token' => 'user_token#create'
-      post 'sign_up' => 'user_token#create'
+      post 'login' => 'user_token#create'
       resources :books, except: :destroy
     end
   end

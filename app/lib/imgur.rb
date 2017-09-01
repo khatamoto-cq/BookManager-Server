737bada91d5f7bfcf370d9c57ace7a488983030e
khatamoto-cq <k.hatamoto@caraquri.com>
2017-09-01 15:31:13 +0900

    Imgurの処理をlibに切り出した。

---
+++ app/lib/imgur.rb
@@ -0,0 +1,13 @@
+class ImgurClient
+  IMGUR_URL = 'https://api.imgur.com/3/image'.freeze
+  IMGUR_CLIENT_ID = 'ae6353455c3635c'.freeze
+
+  def self.post(encorded_image)
+    response = RestClient.post(IMGUR_URL, { image: encorded_image, type: 'base64' },
+                             { Authorization: "Client-ID #{IMGUR_CLIENT_ID}"})
+    return nil unless response.code == 200
+
+    json = JSON.parse(response)
+    json['data']['link']
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ApplicationRecord
-  IMAGUR_URL = 'https://api.imgur.com/3/image'.freeze
-  IMGUR_CLIENT_ID = 'ae6353455c3635c'.freeze
-
   attr_accessor :image_data
+
   belongs_to :user
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
@@ -12,16 +10,6 @@ class Book < ApplicationRecord
 
   def upload_image
     return if image_data.nil?
-    self.image_url = post(image_data)
+    self.image_url = Imgur.post(image_data)
   end
-
-  private
-    def post(encorded_image)
-      response = RestClient.post(IMAGUR_URL, { image: encorded_image, type: 'base64' },
-                               { Authorization: "Client-ID #{IMGUR_CLIENT_ID}"})
-      return nil unless response.code == 200
-
-      json = JSON.parse(response)
-      return json['data']['link']
-    end
 end

9c803daed166cd7e58ad24b3690bebe628eb11cc
khatamoto-cq <k.hatamoto@caraquri.com>
2017-09-01 18:10:58 +0900

    Imgurのclient_idを環境変数から読むように修正

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'kaminari'
 gem 'date_validator'
 gem 'email_validator'
 gem 'rest-client'
+gem 'dotenv-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/lib/imgur.rb
+++ app/lib/imgur.rb
@@ -1,10 +1,9 @@
 class Imgur
   IMGUR_URL = 'https://api.imgur.com/3/image'.freeze
-  IMGUR_CLIENT_ID = 'ae6353455c3635c'.freeze
 
   def self.post(encorded_image)
     response = RestClient.post(IMGUR_URL, { image: encorded_image, type: 'base64' },
-                             { Authorization: "Client-ID #{IMGUR_CLIENT_ID}"})
+                               { Authorization: "Client-ID #{Rails.application.secrets.imgur_client_id}"})
     return nil unless response.code == 200
 
     json = JSON.parse(response)
